a4b04af1ea800525e5e32f94ba206a87230cd827
whchang10 <whchang10@gmail.com>
2018-03-17 13:29:45 -0700

    Remove Firebase test code

--- code/app/src/main/java/com/app/dueday/maya/MainActivity.java
+++ code/app/src/main/java/com/app/dueday/maya/MainActivity.java
@@ -35,31 +35,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Write a message to the database
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("All messages");
-
-        myRef.child("message").setValue("Hello, World!");
-        DatabaseReference hey = myRef.child("hey");
-        hey.setValue("Hey, World!");
-
-
-        // Read from the database
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                @SuppressWarnings("unchecked")
-                Map<String, String> map = (Map)dataSnapshot.getValue();
-                Log.d(TAG, "Value is: " + map);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
     }
 }
