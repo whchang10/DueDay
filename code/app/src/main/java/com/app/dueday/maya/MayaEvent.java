36c333f211750233eb0c8f85fc4a0b33d75b3a9b
whchang10 <whchang10@gmail.com>
2018-03-19 11:40:29 -0700

    Remove group event class

--- code/app/src/main/java/com/app/dueday/maya/MayaEvent.java
+++ code/app/src/main/java/com/app/dueday/maya/MayaEvent.java
@@ -2,43 +2,53 @@ package com.app.dueday.maya;
 
 
 import java.util.Date;
+import java.util.List;
 
 public class MayaEvent {
     public String id;
     public String name;
     public String location;
     public String details;
+    public boolean mPersonal;
 
     public Date beginTime;
     public Date endTime;
+
+    public String projectID;
+    public List<User> attendeeCollection;
 
     public MayaEvent() {
 
     }
 
-    public MayaEvent(String name, String location, String details) {
+    public MayaEvent(String name, String location, String details, boolean personal) {
         this.name = name;
         this.location = location;
         this.details = details;
+        this.mPersonal = personal;
     }
 
-    public MayaEvent(String name, String location, String details, Date beginTime, Date endTime) {
-        this(name, location, details);
+    public MayaEvent(String name, String location, String details, boolean personal, Date beginTime, Date endTime) {
+        this(name, location, details, personal);
 
         this.beginTime = beginTime;
         this.endTime = endTime;
     }
 
     public boolean isPersonalEvent() {
-        return true;
-    }
-
-    public boolean isGroupEvent() {
-        return false;
+        return mPersonal;
     }
 
     public void setDuration(Date beginTime, Date endTime) {
         this.beginTime = beginTime;
         this.endTime = endTime;
+    }
+
+    public void addAttendee(User user) {
+        attendeeCollection.add(user);
+    }
+
+    public void removeAttendee(User user) {
+        attendeeCollection.remove(user);
     }
 }
