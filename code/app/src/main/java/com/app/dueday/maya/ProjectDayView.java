68f4002f35972fd22db0ab022830170370bfc585
whchang10 <whchang10@gmail.com>
2018-03-20 11:59:21 -0700

    Remove unused code

--- code/app/src/main/java/com/app/dueday/maya/ProjectDayView.java
+++ code/app/src/main/java/com/app/dueday/maya/ProjectDayView.java
@@ -23,57 +23,9 @@ import java.util.List;
  */
 public class ProjectDayView extends BaseActivity {
     private List<WeekViewEvent> events;
-    private List<MayaEvent> eventCollection;
 
     private Project mProject;
     private List<User> membersCollection;
-
-    private void readMayaEvent() {
-        FirebaseUtil.getCurrentUserEventListRef().addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    GenericTypeIndicator<List<MayaEvent>> genericTypeIndicator = new GenericTypeIndicator<List<MayaEvent>>() {
-                    };
-                    eventCollection = dataSnapshot.getValue(genericTypeIndicator);
-                    events.clear();
-                    int id = 0;
-                    for (MayaEvent mayaEvent : eventCollection) {
-                        Calendar startTime = Calendar.getInstance();
-                        startTime = Calendar.getInstance();
-                        startTime.set(Calendar.DATE, mayaEvent.beginTime.day);
-                        startTime.set(Calendar.HOUR_OF_DAY, mayaEvent.beginTime.hourOfDay);
-                        startTime.set(Calendar.MINUTE, mayaEvent.beginTime.minute);
-                        startTime.set(Calendar.MONTH, mayaEvent.beginTime.month - 1);
-                        startTime.set(Calendar.YEAR, mayaEvent.beginTime.year);
-                        Calendar endTime = Calendar.getInstance();
-                        endTime = Calendar.getInstance();
-                        endTime.set(Calendar.DATE, mayaEvent.endTime.day);
-                        endTime.set(Calendar.HOUR_OF_DAY, mayaEvent.endTime.hourOfDay);
-                        endTime.set(Calendar.MINUTE, mayaEvent.endTime.minute);
-                        endTime.set(Calendar.MONTH, mayaEvent.endTime.month - 1);
-                        endTime.set(Calendar.YEAR, mayaEvent.endTime.year);
-                        WeekViewEvent event = new WeekViewEvent(id++, mayaEvent.name + "\n\n", "Location: " + mayaEvent.location + "\n\n" + " Descriptions: " + mayaEvent.details, startTime, endTime);
-                        if (!mayaEvent.personalEvent) {
-                            event.setColor(getResources().getColor(R.color.event_color_04));
-                        } else {
-                            event.setColor(getResources().getColor(R.color.event_color_03));
-                        }
-
-                        events.add(event);
-                    }
-                    Log.d(UIUtil.TAG, "get data success");
-                    getWeekView().notifyDatasetChanged();
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(UIUtil.TAG, "Failed to read value.", error.toException());
-            }
-        });
-    }
 
     private void prepareEvents() {
         events.clear();
@@ -125,8 +77,6 @@ public class ProjectDayView extends BaseActivity {
         mProject = (Project) getIntent().getSerializableExtra(ProjectCalendar.EXTRA_PROJECT);
         membersCollection = (ArrayList<User>) getIntent().getSerializableExtra(ProjectCalendar.EXTRA_MEMBERS_COLLECTION);
 
-
-        //readMayaEvent();
         prepareEvents();
     }
 
