a430e08f1c24948e248471b98774985abd6d72e3
whchang10 <whchang10@gmail.com>
2018-03-20 11:57:20 -0700

    Implrmrnt Project DayView

--- code/app/src/main/java/com/app/dueday/maya/ProjectDayView.java
+++ code/app/src/main/java/com/app/dueday/maya/ProjectDayView.java
@@ -75,6 +75,49 @@ public class ProjectDayView extends BaseActivity {
         });
     }
 
+    private void prepareEvents() {
+        events.clear();
+        int id = 0;
+        for (User user : membersCollection) {
+            for (MayaEvent mayaEvent : user.eventCollection) {
+                Calendar startTime = Calendar.getInstance();
+                startTime = Calendar.getInstance();
+                startTime.set(Calendar.DATE, mayaEvent.beginTime.day);
+                startTime.set(Calendar.HOUR_OF_DAY, mayaEvent.beginTime.hourOfDay);
+                startTime.set(Calendar.MINUTE, mayaEvent.beginTime.minute);
+                startTime.set(Calendar.MONTH, mayaEvent.beginTime.month - 1);
+                startTime.set(Calendar.YEAR, mayaEvent.beginTime.year);
+                Calendar endTime = Calendar.getInstance();
+                endTime = Calendar.getInstance();
+                endTime.set(Calendar.DATE, mayaEvent.endTime.day);
+                endTime.set(Calendar.HOUR_OF_DAY, mayaEvent.endTime.hourOfDay);
+                endTime.set(Calendar.MINUTE, mayaEvent.endTime.minute);
+                endTime.set(Calendar.MONTH, mayaEvent.endTime.month - 1);
+                endTime.set(Calendar.YEAR, mayaEvent.endTime.year);
+
+                WeekViewEvent event;
+                // Current user personal event
+                if (FirebaseUtil.getCurrentUser().id.equals(user.id) && !mProject.id.equals(mayaEvent.projectID)) {
+                    event = new WeekViewEvent(id++, mayaEvent.name + "\n\n", "Location: " + mayaEvent.location + "\n\n" + " Descriptions: " + mayaEvent.details, startTime, endTime);
+                    event.setColor(getResources().getColor(R.color.event_color_03));
+                    events.add(event);
+                }
+                // Current project event
+                else if (FirebaseUtil.getCurrentUser().id.equals(user.id) && mProject.id.equals(mayaEvent.projectID)){
+                    event = new WeekViewEvent(id++, mayaEvent.name + "\n\n", "Location: " + mayaEvent.location + "\n\n" + " Descriptions: " + mayaEvent.details, startTime, endTime);
+                    event.setColor(getResources().getColor(R.color.event_color_04));
+                    events.add(event);
+                }
+                // Other person event exclude current project event
+                else if (!FirebaseUtil.getCurrentUser().id.equals(user.id) && !mProject.id.equals(mayaEvent.projectID)) {
+                    event = new WeekViewEvent(id++, "Private" + "\n\n", "No Information", startTime, endTime);
+                    event.setColor(getResources().getColor(R.color.event_color_01));
+                    events.add(event);
+                }
+            }
+        }
+    }
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         events = new ArrayList<WeekViewEvent>();
@@ -83,7 +126,8 @@ public class ProjectDayView extends BaseActivity {
         membersCollection = (ArrayList<User>) getIntent().getSerializableExtra(ProjectCalendar.EXTRA_MEMBERS_COLLECTION);
 
 
-        readMayaEvent();
+        //readMayaEvent();
+        prepareEvents();
     }
 
     @Override
